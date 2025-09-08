828ae567dedd9ef7c2ecdcadef83699c22ff3a84
Your Name <your.email@example.com>
2025-09-08 22:03:00 +0530

    Merge branch 'main' of https://github.com/11AN-CODE/Forssee-screening-task-1-ui-ux

--- statistics_app/models.py
+++ statistics_app/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+# In statistics_app/models.py
 
-# Create your models here.
+from workshop_app.models import Profile 
+# ... and any other models you are using in this file
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
